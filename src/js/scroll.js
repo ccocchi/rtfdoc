5cfa01b5d8feaee18a2620d549d53d9df925ae36
ccocchi <cocchi.c@gmail.com>
2020-07-28 23:39:15 +0200

    Handle clicks/anchors

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -8,12 +8,19 @@ let current = null,
     currentLink = null;
 
 function onScroll() {
-  const sectionOnScreen = sections.find(s => s.offsetTop > 0 && s.offsetTop <= node.scrollTop + 50);
+  const sectionOnScreen = sections.find(s => s.offsetTop > 0 && s.offsetTop <= node.scrollTop + 100);
 
   if (sectionOnScreen.id !== current) {
-    if (currentLink) currentLink.classList.remove('current')
+    if (currentLink) {
+      currentLink.classList.remove('current');
+      if (currentLink.classList.contains('expandable')) currentLink.parentElement.classList.remove('current');
+    }
+
     const node = document.querySelector(`nav a[href="#${sectionOnScreen.id}"]`);
-    if (node) node.classList.add('current');
+    if (node) {
+      node.classList.add('current');
+      if (node.classList.contains('expandable')) node.parentElement.classList.add('current');
+    }
 
     current = sectionOnScreen;
     currentLink = node;
